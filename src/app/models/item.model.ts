b6227da11f2c01a4a8855cfd281d95b3b6e43747
Bret Dalldorf <bret.dalldorf@gmail.com>
2018-11-17 13:47:23 -0800

    refactor(itemModel): change from interface to class

--- src/app/models/item.model.ts
+++ src/app/models/item.model.ts
@@ -1,19 +1,7 @@
 import { ID } from '@datorama/akita';
 
-export interface Item {
+export class Item {
     id: ID;
     itemname: string;
     itemprice: number;
-}
-
-export function creatItem({
-  id = null,
-  itemname = '',
-  itemprice = 0.00
-}: Partial<Item>) {
-  return {
-    id,
-    itemname,
-    itemprice
-  } as Item;
 }
