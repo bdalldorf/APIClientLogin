64d62b2f492bdc6e2d9ddb217572dabdbc44c959
Bret Dalldorf <bret.dalldorf@gmail.com>
2019-01-08 20:25:53 -0800

    refactor(error.interceptor): fixed equals conditional

--- src/app/core/interceptors/error.interceptor.ts
+++ src/app/core/interceptors/error.interceptor.ts
@@ -11,7 +11,7 @@ export class ErrorInterceptor implements HttpInterceptor {
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         return next.handle(request).pipe(catchError(err => {
-            if (err.status === 401 || err.status == 400) {
+            if (err.status === 401 || err.status === 400) {
                 // auto logout if 401 response returned from api
                 // auto logout if 400 response returned from api
                 this.authenticationService.logout();
